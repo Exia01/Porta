719fd97364c75e3f8d75295d5a89c6b2a0213bbc
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-18 14:01:07 -0500

    update packages, update components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from './hoc/Layout/Layout'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Layout></Layout>
     </div>
   );
 }

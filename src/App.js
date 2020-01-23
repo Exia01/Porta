17333c5eaaae98036c23bbc991f05b2bcd030c4f
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-22 20:21:22 -0500

    add font awesome to appJS. Update MainContent container

--- src/App.js
+++ src/App.js
@@ -7,13 +7,21 @@ import Spinner from './components/UI/Spinner/Spinner';
 import Layout from './hoc/Layout/Layout';
 import MainContent from './containers/MainContent';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+  faEnvelope,
+  faFileAlt,
+} from '@fortawesome/free-solid-svg-icons';
+library.add(fab, faEnvelope, faFileAlt); //Adding to libray/ will use on footer
+
 function App() {
   return (
     <div className='App'>
       <Layout>
         <React.Suspense fallback={<Spinner />}>
           <Switch>
-          <Route path='/' exact component={MainContent} />
+            <Route path='/' exact component={MainContent} />
             <Redirect to='/' />
           </Switch>
         </React.Suspense>
--- src/containers/MainContent.js
+++ src/containers/MainContent.js
@@ -1,42 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classes from '../CSSModules/MainContent.module.css';
 import axios from 'axios';
 import Project from '../components/Project/Project';
 import Skills from '../components/Skills/Skills';
+
 export class MainContent extends Component {
   state = {
-    data: {}
+    //will add data by api
+    data: null,
+    loading: true,
   };
 
-  async getResumeData() {
-    try {
-      //   Load async data from an inexistent endpoint.
-      let resumeData = await axios.get('/resumeData.json');
-      console.log(resumeData);
-      this.setState(resumeData);
-    } catch (e) {
-      console.log(`😱 Axios request failed: ${e}`);
-    }
+  getResumeData() {
+    axios
+      .get('/resumeData.json')
+      .then(resumeData => {
+        // handle success
+        this.setState({data: resumeData.data, loading: false});
+      })
+      .catch(function (e) {
+        console.log(`😱 Axios request failed: ${e}`);
+      });
   }
 
-  async componentDidMount() {
+  onInitPortfolioRender() {
+
+  }
+
+  componentDidMount() {
     this.getResumeData();
   }
 
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    //figure out how to cancel axios call here
+  }
 
   render() {
-    let projects;
-    if (this.state.data.portfolio) {
-      projects = this.state.data.portfolio.projects.map(projectData => {
-        return <Project projectData={projectData} key={projectData.title} />;
-      });
-    } else {
+    let projects
+    if (this.state.data === null) {
       projects = (
         <div className={classes.projectsError}>
           <h1>No Projects Available</h1>
         </div>
       );
+    } else {
+      projects = this.state.data.portfolio.projects.map(projectData => {
+        return <Project projectData={projectData} key={projectData.title} />;
+      })
     }
 
     return (
@@ -53,13 +63,15 @@ export class MainContent extends Component {
           </div>
           {projects}
         </section>
-
-        <section>
-          <Skills />
-        </section>
+        {this.state.data !== null ? <Skills skills={this.state.data.resume.skills} /> : null}
       </React.Fragment>
     );
   }
 }
 
 export default MainContent;
+
+// <Skills skills={this.state.data.resume.skills}/>
+
+
+// https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html

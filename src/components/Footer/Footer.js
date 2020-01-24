1627e694cc6bea9dabc4a4d334a72db06de01627
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-23 21:21:35 -0500

    Update Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,34 @@
 import React from 'react';
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import classes from '../../CSSModules/Footer.module.css';
-
 
 let combineClasses = [classes.footerGridSection, 'container'].join(' ');
 
 const Footer = () => {
   return (
-    <footer className={classes.footer}>
-      <section className={combineClasses}>
-        <a href='http://' target='_blank' rel='noopener noreferrer'>
-          <FontAwesomeIcon icon='envelope'  />
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon='file-alt'  />
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={['fab', 'github']}  />
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={['fab', 'linkedin']}  />
-        </a>
-      </section>
-    </footer>
+    <React.Fragment>
+      <footer className={classes.footer}>
+        <section className={combineClasses}>
+          <a href='http://' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon='envelope' />
+          </a>
+          <a href='http://' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon='file-alt' />
+          </a>
+          <a href='http://' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </a>
+          <a href='http://' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          </a>
+        </section>
+      </footer>
+      <div className={classes.credits}>
+        <h4>&copy; 2020 Jose Gonzalez</h4>
+      </div>
+    </React.Fragment>
   );
 };
 
